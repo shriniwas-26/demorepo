932c4b882320136b4cab89238b741a7da0ba8a34
Shriniwas Pawar <pawar.shriniwas26@gmail.com>
2025-07-02 21:05:36 +0530

    new commit

--- node/app.js
+++ node/app.js
@@ -1,4 +1,15 @@
+const http = require('http');
+
+
 function sayHello(str) {
   console.log("Hello" + str);
 }
 sayHello("Docker")
+
+const server = http.createServer((req, res) => {
+  res.end('Hello Docker');
+});
+
+server.listen(8000, () => {
+  console.log('Server running on port 8000');
+});
